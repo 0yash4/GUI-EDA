5f57cc6a0a711c246a804f22781eb02dcd4bbce2
Yash Choudhary <4yashchoudhary@gmail.com>
2024-03-15 13:13:49 -0500

    rough commit to stay secure

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import *
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html')
 
-@app.route("/about")
+
+@app.route("/EDA")
 def about():
-    return "<p>now we're in about say hi bitch</p>"
+    return render_template('EDA.html')
+
 
 if __name__=="__main__":
     app.run(debug=True, port=8501)
