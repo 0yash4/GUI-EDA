c32fa148a4374b005f0f175e0cdf86e9bd3fc479
Yash Choudhary <4yashchoudhary@gmail.com>
2024-03-15 17:42:20 -0500

    EOD

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
+from src.logger import logging
+
+
 from flask import *
+from wtforms import SubmitField
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+import os
 
+    
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a random string'
+app.config['UPLOAD_FOLDER'] = "static/files"
 
-@app.route("/")
-@app.route("/home")
+class uploadFile(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route("/", methods = ['GET', 'POST'])
+@app.route("/home", methods = ['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = uploadFile()
+   
+    if form.validate_on_submit():
+        f = form.file.data
+        filename = secure_filename(f.filename)                
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        f.save(file_path)
+        return redirect(url_for('home'))
+    
+        
+    return render_template('home.html', form=form)
 
 
 @app.route("/EDA")
