f1dc86b6fc9288bcffcc32bc703705e74635a5ac
Yash Choudhary <4yashchoudhary@gmail.com>
2024-03-17 06:37:46 -0500

    random commit

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import streamlit as st 
+import pandas as pd
+import os
+from src.logger import logging
 
+st.sidebar.title("GUI-EDA")
 
-st.title("GUI-EDA")
-st.header("Header")
-st.subheader("Sub Header")
-st.markdown("""
-            # :smile: 
-            ## H2
-            ### H3
-            
-            
-            :moon: 
-            """, True)
+data_file = st.sidebar.file_uploader("Upload file", type=["csv", "xlsx", "json"], help="Upload your data file here")
+
+if data_file is not None:
+    st.sidebar.write(f"{data_file.name} Uploaded Successfully!")
+    if st.sidebar.button("Save File to Data Folder", key="save_button"):
+        # Create a directory if it doesn't exist
+        os.makedirs("data", exist_ok=True)
+        
+        # Save the file to the data folder
+        with open(os.path.join("data", data_file.name), "wb") as f:
+            f.write(data_file.getvalue())
+        st.sidebar.write("File saved successfully!")
