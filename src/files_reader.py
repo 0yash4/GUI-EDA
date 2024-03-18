392966593b1baca694cdecaef6094d79430b2dc4
Yash Choudhary <4yashchoudhary@gmail.com>
2024-03-18 04:28:07 -0500

    precautionary

--- src/files_reader.py
+++ src/files_reader.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 from logger import logging
-
+from custom_exception import CustomException
+import sys
 
 import pandas as pd
 import os
@@ -20,19 +22,33 @@ def file_extension():
         list.append(split_tup[1])
     return list 
     
-
+@dataclass
 class file_reader():
     def __init__(self):
-        os.makedirs(data_folder, exist_ok=True)
-        for i in range(0, len(file_extension())):
-            if file_extension()[i] == "csv":
-                pd.read_csv(f"{data_folder}/{file_names[i]}")
-                logging.info("Read CSV file")
-                
-        for i in range(0, len(file_extension())):
-            if file_extension()[i] == "json":
-                pd.read_json(f"{data_folder}/{file_names[i]}")
-                logging.info("Read Json file")
-                
-a=file_reader()
+        try:
+            os.makedirs(data_folder, exist_ok=True)
+            for i in range(0, len(file_extension())):
+                if file_extension()[i] == "csv":
+                    pd.read_csv(f"{data_folder}/{file_names[i]}")
+                    logging.info(f"Read {file_names[i]}")
+                    
+            for i in range(0, len(file_extension())):
+                if file_extension()[i] == "json":
+                    pd.read_json(f"{data_folder}/{file_names[i]}")
+                    logging.info(f"Read {file_names[i]}")
+                    
+            for i in range(0, len(file_extension())):
+                if file_extension()[i] == "xlsx":
+                    pd.read_json(f"{data_folder}/{file_names[i]}")
+                    logging.info(f"Read {file_names[i]}")
+        
+        except Exception as e:
+            CustomException(e, sys)
+            logging.info(e)
 
+
+
+
+
+if __name__=="__main__":
+    file_reader = file_reader()
