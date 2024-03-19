831d7423d00a8646dfae14e439827fe7a0c8a367
Yash Choudhary <4yashchoudhary@gmail.com>
2024-03-19 15:25:00 -0500

    solving a problem that was not a problem

--- src/files_reader.py
+++ src/files_reader.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from logger import logging
 from custom_exception import CustomException
 import sys
@@ -7,44 +6,47 @@ import pandas as pd
 import os
 
 data_folder = "data"
-file_names = os.listdir(data_folder)
-
-
-#function to read file name and extension
+files_names_list = os.listdir(data_folder)   #Gives the list of all the files available in data folder
 
 
 #Below funtion will return the List[str] of file extension
-def file_extension():
+
+def file_nameOnly():   #This function will give the file name only without extension
     
     list=[] 
-    for file in file_names:
+    for file in files_names_list:
+        split_tup = file.split(".")
+        list.append(split_tup[0])
+    return list 
+
+def file_extension():       #This function will give the file name only without extension
+    
+    list=[] 
+    for file in files_names_list:
         split_tup = file.split(".")
         list.append(split_tup[1])
     return list 
-    
-@dataclass
+
 class file_reader():
     def __init__(self):
-        try:
             os.makedirs(data_folder, exist_ok=True)
             for i in range(0, len(file_extension())):
                 if file_extension()[i] == "csv":
-                    pd.read_csv(f"{data_folder}/{file_names[i]}")
-                    logging.info(f"Read {file_names[i]}")
-                    
-            for i in range(0, len(file_extension())):
-                if file_extension()[i] == "json":
-                    pd.read_json(f"{data_folder}/{file_names[i]}")
-                    logging.info(f"Read {file_names[i]}")
-                    
-            for i in range(0, len(file_extension())):
-                if file_extension()[i] == "xlsx":
-                    pd.read_json(f"{data_folder}/{file_names[i]}")
-                    logging.info(f"Read {file_names[i]}")
+                    pd.read_csv(f"{data_folder}/{files_names_list[i]}")
+                    logging.info(f"Read {files_names_list[i]}")
+
+                elif file_extension()[i] == "json":
+                    pd.read_json(f"{data_folder}/{files_names_list[i]}")
+                    logging.info(f"Read {files_names_list[i]}")
+                
+                else:    
+                    pd.read_excel(f"{data_folder}/{files_names_list[i]}")
+                    logging.info(f"Read {files_names_list[i]}")
+
+            
+    def head(self, files_names_list):
+        pass
         
-        except Exception as e:
-            CustomException(e, sys)
-            logging.info(e)
 
 
 
